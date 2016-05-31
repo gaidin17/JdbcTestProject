dd7104507553d0b1beb34e6919bf58bc1388dc01
Evgeny Akulenko <gaidin17@yandex.ru>
2016-05-31 10:57:57 +0300

    add logger

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -2,10 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 /**
  * Created by Evgeny_Akulenko on 4/28/2016.
  */
 public class DBConnector {
+
+    private final Logger logger = LoggerFactory.getLogger(DBConnector.class);
 
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
     private static final String USERNAME = "SOUTHWIND";
@@ -13,11 +18,12 @@ public class DBConnector {
     private Connection connection;
 
     public DBConnector() {
+
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Connection to jdbc:oracle:thin:@localhost:1521:XE is open");
+            logger.info("INFO: Connection to {} is open", connection);
         } catch (SQLException ex) {
-            System.out.println("Not connected!");
+            logger.error("ERROR: No connection to {} ", URL);
         }
     }
 
@@ -25,14 +31,15 @@ public class DBConnector {
         try {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
-                System.out.println("Connection to " + URL + " is closed.");
+                logger.info("INFO: Connection to {} is closed", URL);
             }
         } catch (SQLException ex) {
-
+            logger.error("ERROR: {}", ex.getErrorCode());
         }
     }
 
     public Connection getConnection() {
+        logger.warn("WARN: getting connection");
         return connection;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,39 @@
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Evgeny_Akulenko on 4/28/2016.
  */
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         DriverRegistrator.registerDriver();
         String query = "SELECT * FROM ORDER_DETAILS";
         DBConnector connector = new DBConnector();
         DBWorker dbWorker = null;
         try {
+            logger.info("INFO: Query: {}", query);
             dbWorker = new DBWorker(connector.getConnection(), query);
+            logger.info("INFO: Set DBWorker: {}", dbWorker);
             dbWorker.printResultToConsol();
-
         } catch (SQLException ex) {
-
+            logger.error("Error: {}", ex.getStackTrace());
         } catch (Exception ex) {
-
+            logger.error("Error: {}", ex.getStackTrace());
         } finally {
+            logger.warn("Warn: closing connection:");
             try {
                 if (dbWorker != null) {
                     dbWorker.closeResultSet();
                     dbWorker.closeStatement();
                 }
             } catch (SQLException ex) {
+                logger.error("Error: {}", ex.getStackTrace());
 
             } catch (Exception ex) {
-
+                logger.error("Error: {}", ex.getStackTrace());
             }
             connector.closeConnection();
         }
